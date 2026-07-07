2bb3f3d5711e090806202e788626fbf52d9c7270
m.umair.kn <m.umair.kn@gmail.com>
2026-07-07 16:00:59 +0500

    B: OpenRouter API minimal working example

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from build-an-ai-agent!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
+def main() -> None:
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key == None:
+        raise RuntimeError("API key not found")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key
+    )
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ]
+    )
+
+    print("Response:")
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
