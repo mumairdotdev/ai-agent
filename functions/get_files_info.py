6867d51e2870a93b9eba3fa893bdc0604c996370
m.umair.kn <m.umair.kn@gmail.com>
2026-07-08 11:23:25 +0500

    I: Added listing files feature

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,12 +4,21 @@ import os
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
         working_directory_abs = os.path.abspath(working_directory)
-        if not os.path.isdir(directory):
-            raise Exception(f'"{directory}" is not a directory')
         target_dir = os.path.normpath(os.path.join(working_directory_abs, directory))
         if os.path.commonpath([working_directory_abs, target_dir]) != working_directory_abs:
             raise Exception(f'Cannot list "{directory}" as it is outside the permitted working directory')
-        return f'Success: "{directory}" is within the working directory'
+        if not os.path.isdir(target_dir):
+            raise Exception(f'"{directory}" is not a directory')
+        # return f'Success: "{directory}" is within the working directory'
+        files_info: list[str] = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
     except Exception as e:
         return f"Error: {e}"
 
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -4,7 +4,7 @@ def test():
     print(get_files_info("calculator", "."))
     print(get_files_info("calculator", "/bin"))
     print(get_files_info("calculator", "../"))
-    print(get_files_info("calculator", "main.py"))
+    print(get_files_info("calculator", "pkg"))
 
 if __name__ == "__main__":
     test()
